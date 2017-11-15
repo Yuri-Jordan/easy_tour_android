316b62c53dfe3185abebad871ec8c5619271b8a6
yuri <yurijrdn@gmail.com>
2017-11-15 16:37:51 -0300

    Integra da telas de autenticação com o restante do app

--- app/src/main/java/com/example/cris/easytourbrasil/app/LoginActivity.java
+++ app/src/main/java/com/example/cris/easytourbrasil/app/LoginActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.cris.easytourbrasil.MainActivity;
+import com.example.cris.easytourbrasil.MapsActivity;
 import com.example.cris.easytourbrasil.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView linkCadastro;
+    Button acessarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,15 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), CadastroActivity.class));
             }
         });
+
+        acessarButton = (Button) findViewById(R.id.acessarButton);
+        acessarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), MainActivity.class));
+                finish();
+            }
+        });
+
     }
 }
